cad7f66e6ff4ac3125241fbe43aa8b0084952d5a
juncyan <125954099+juncyan@users.noreply.github.com>
2025-01-16 00:17:17 +0800

    update train.py, predict.py

--- work/predict.py
+++ work/predict.py
@@ -1,16 +1,3 @@
-# Copyright (c) 2023 torchtorch Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import os
 import cv2
@@ -131,14 +118,6 @@ def predict(model, dataset, weight_path=None, data_name="test", num_classes=2):
     
 
 def test(model, dataset, args):
-    """
-    Launch evalution.
-
-    Args:
-        model（nn.Layer): A semantic segmentation model.
-        dataset (torch.io.DataLoader): Used to read and process test datasets.
-        weights_path (string, optional): weights saved local.
-    """
     
     if args.best_model_path:
         layer_state_dict = torch.load(f"{args.best_model_path}")
@@ -239,115 +218,5 @@ def test(model, dataset, args):
         data = np.array(data)
         pd.DataFrame(data).to_csv(os.path.join(img_dir, f'{args.model_name}_violin.csv'), header=['TN', 'TP', 'FP', 'FN'], index=False)
 
-
-
-def test_last(model, dataset, args, last_weight_path=None):
-    """
-    Launch evalution.
-
-    Args:
-        model（nn.Layer): A semantic segmentation model.
-        dataset (torch.io.DataLoader): Used to read and process test datasets.
-        weights_path (string, optional): weights saved local.
-    """
-    
-    if last_weight_path != None:
-        layer_state_dict = torch.load(last_weight_path)
-        model.load_state_dict(layer_state_dict)
-    else:
-        exit()
-    
-
-    time_flag = datetime.datetime.strftime(datetime.datetime.now(), r"%Y_%m_%d_%H")
-
-    img_dir = f"/mnt/data/Results/{args.data_name}/{args.model_name}_{time_flag}_last"
-    if not os.path.isdir(img_dir):
-        os.makedirs(img_dir)
-
-
-    logger = load_logger(f"{img_dir}/prediction.log")
-    logger.info(f"test {args.model_name} on {args.data_name}")
-   
-    color_label = np.array([[0,0,0],[255,255,255],[0,128,0],[0,0,128]])
-
-
-    evaluator = Metrics(num_class=args.num_classes)
-
-    with torch.no_grad():
-        for _, (img1, img2, label, name) in enumerate(dataset):
-    
-            label = label.cuda()
-            img1 = img1.cuda()
-            img2 = img2.cuda()
-           
-            pred = model(img1, img2)
-            
-            if hasattr(model, "predict"):
-                pred = model.predict(pred)
-            elif hasattr(model, "prediction"):
-                pred = model.prediction(pred)
-            else:
-                if (type(pred) == tuple) or (type(pred) == list):
-                    pred = pred[args.pred_idx]
-
-            evaluator.add_batch(pred, label)
-
-            if pred.shape[1] > 1:
-                pred = torch.argmax(pred, axis=1)
-            pred = pred.squeeze()
-
-            if label.shape[1] > 1:
-                label = torch.argmax(label, axis=1)
-            
-            label = label.squeeze()
-            label = label.cpu().numpy()
-
-            for idx, ipred in enumerate(pred):
-                ipred = ipred.cpu().numpy()
-                if (np.max(ipred) != np.min(ipred)):
-                    flag = (label[idx] - ipred)
-                    ipred[flag == -1] = 2
-                    ipred[flag == 1] = 3
-                    img = color_label[ipred]
-                    cv2.imwrite(f"{img_dir}/{name[idx]}", img)
-
-    evaluator.calc()
-    miou = evaluator.Mean_Intersection_over_Union()
-    acc = evaluator.Pixel_Accuracy()
-    class_iou = evaluator.Intersection_over_Union()
-    class_precision = evaluator.Class_Precision()
-    kappa = evaluator.Kappa()
-    recall = evaluator.Mean_Recall()
-    class_dice = evaluator.Dice()
-    macro_f1 = evaluator.Macro_F1()
-    class_recall = evaluator.Recall()
-    # print(batch_cost, reader_cost)
-
-    _,c,w,h = img1.shape
-    x= torch.rand([1,c,w,h]).cuda()
-    flops, params = profile(model, [x,x])
-    
-    infor = "[PREDICT] #Images: {} batch_cost {:.4f}, reader_cost {:.4f}".format(len(dataset), 0, 0)
-    logger.info(infor)
-    infor = "[METRICS] mIoU: {:.4f}, Acc: {:.4f}, Kappa: {:.4f}, recall: {:.4f}, Macro_F1: {:.4f}".format(
-            miou, acc, kappa, recall, macro_f1)
-    logger.info(infor)
-
-    logger.info("[METRICS] Class IoU: " + str(np.round(class_iou, 4)))
-    logger.info("[METRICS] Class Precision: " + str(np.round(class_precision, 4)))
-    logger.info("[METRICS] Class Recall: " + str(np.round(class_recall, 4)))
-    logger.info("[METRICS] Class Dice: " + str(np.round(class_dice, 4)))
-    logger.info(f"[PREDICT] model flops is {int(flops)}, params is {int(params)}")
-
-    img_files = glob.glob(os.path.join(img_dir, '*.png'))
-    data = []
-    for img_path in img_files:
-        img = cv2.imread(img_path)
-        lab = cls_count(img)
-        # lab = np.argmax(lab, -1)
-        data.append(lab)
-    if data != []:
-        data = np.array(data)
-        pd.DataFrame(data).to_csv(os.path.join(img_dir, f'{args.model_name}_violin.csv'), header=['TN', 'TP', 'FP', 'FN'], index=False)
 
 
--- work/train.py
+++ work/train.py
@@ -15,7 +15,6 @@ from work.utils import get_scheduler
 
 def loss(logits,labels):
     # return nn.BCEWithLogitsLoss()(logits, lab)
-
     if logits.shape == labels.shape:
         labels = torch.argmax(labels,dim=1)
     elif len(labels.shape) == 3:
@@ -24,7 +23,6 @@ def loss(logits,labels):
         assert "pred.shape not match label.shape"
     #logits = F.softmax(logits,dim=1)
     return torch.nn.CrossEntropyLoss()(logits,labels)
-
 
 def dice_loss(logits,targets,smooth=1e-5):
     # logic.shape=[C,N,H,W],target.shape=[C,N,H,W]&[C,1,H,W]
@@ -48,40 +46,20 @@ def dice_loss(logits,targets,smooth=1e-5):
 def train(model, dataloader_train, dataloader_eval, dataloader_test, args):
 
     args.logger.info("start train")
-    #optimizer = optim.SGD(TNet.parameters(), opt_params)
-    # optimizer = optim.SGD(
-    #     params=[
-    #         {'params': get_params(model, key='1x'), 'lr': 5e-4},
-    #         {'params': get_params(model, key='10x'), 'lr': 5e-3}
-    #     ],
-    #     momentum = 0.9 , weight_decay = 1e-4)
     optimizer = optim.Adam(model.parameters(),lr= 5e-4, betas=(0.9, 0.999))
-    # torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0, T_mult = 1, eta_min = 0, last_epoch = -1, verbose = False)
     
     max_itr = args.iters * len(dataloader_train)
 
     lr_step = get_scheduler(optimizer, max_itr, 'step')
     max_miou = 0.
     best_iter = 0
-    #early_stopping = Early_stopping(eps=2e-5,llen=10)
-    #criterion = SegmentationLosses(weight=None,cuda=True).build_loss("ce")
-
     for epoch in range(args.iters):
-        # if test:
-        #     test_model(model, dataloader_eval, evaluator,args)
-        #     images_prediction(model, dataloader_pred, save_dir=save_folder, label_info=label_info)
-        #     break
-        # np.random.seed(53113)
-        # torch.manual_seed(53113)
-    #
+
         now = datetime.datetime.now()
         model.train()
         loss_record = []
-        #evaluator.reset()
 
         for _,(image1, image2, label) in enumerate(dataloader_train):
-
-            #optimizer = adjust_lr(optimizer, epoch*iter, max_itr)
             
             image1 = image1.cuda()
             image2 = image2.cuda()
@@ -113,27 +91,14 @@ def train(model, dataloader_train, dataloader_eval, dataloader_test, args):
 
         if (epoch+1) % 10 ==0:
              torch.save(model.state_dict(), os.path.join(args.save_dir, "iter_{}.pth".format(epoch+1)))
-        
-        # if miou_eval > max(0.7, max_miou):
-        #     torch.save(model.state_dict(), args.best_model_path)
 
         if miou_eval > max(0.5, max_miou):
             torch.save(model.state_dict(), args.best_model_path)
             max_miou = miou_eval
             best_iter = epoch+1
         args.logger.info("[TRAIN] train time is {:.2f}, best iter {}, max mIoU {:.4f}".format((datetime.datetime.now() - now).total_seconds(), best_iter, max_miou))
-        #print("+++++++++++++++++++++++++++++++time:", datetime.datetime.now() - now)
-        # early_stopping.add_data(loss_tm)
-        # if early_stopping.en_stop(force=True):
-        #     break
-        # test_model(model, dataloader_eval, evaluator)
-        # images_prediction(model, dataloader_pred, save_dir=save_folder, label_info=label_info)
-    
+
     test(model, dataloader_test, args)
-    # last_path = os.path.join(args.save_dir, "iter_{}.pth".format(args.iters))
-    # test_last(model, dataloader_test, args, last_path)
-    
-    # torch.save(TNet.state_dict(),save_folder+"last.path")
 
 
 
